d55cbcd750363e6cb266293ae050e0003290f5d3
dlonack <Lonack@gmail.com>
2020-06-15 12:37:42 -0700

    Commit Module4

---
+++ ON/DaveLonack/Module4/ActionDelegates/ActionDelegates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ActionDelegate
+{
+    class Program
+    {
+        public static void myfun(int p, int q)
+        {
+            Console.WriteLine(p - q);
+        }
+
+        //Main method
+        static public void Main()
+        {
+            //Using action delegate
+            //Here, Action delegate
+            //Contains two input parameters
+            Action<int, int> val = myfun;
+            val(20, 5);
+        }
+    }
+}
