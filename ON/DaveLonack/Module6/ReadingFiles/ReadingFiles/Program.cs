44a310b9d6bafd2572ec47650e9ba777ab0facab
dlonack <Lonack@gmail.com>
2020-06-15 14:08:15 -0700

    Commit Module6

---
+++ ON/DaveLonack/Module6/ReadingFiles/ReadingFiles/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace ReadingFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Create an instance of StreamReader to read from a file.
+                //The using statement also closes the StreamReader.
+                using(StreamReader sr = new StreamReader("C:/Users/Lonac/source/repos/IS375_Spring_2020/ON/DaveLonack/Module6/jamaica.txt"))
+                {
+                    string line;
+
+                    //Read and display lines from the file until
+                    //the end of the file is reached.
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                //let the user know what went wrong.
+
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ ON/DaveLonack/Module6/UserDefinedExceptions/UserDefinedExceptions/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace UserDefinedExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Temperature temp = new Temperature();
+            //rasis an user-defined exception i.e. TempIsZeroException
+            try
+            {
+                temp.showTemp();
+            }
+            //Catch block 1
+            catch (TempIsZeroException e)
+            {
+                Console.WriteLine("TempIsZeroException: {0}", e.Message);
+            }
+        }
+    }
+}
+
+public class TempIsZeroException : Exception
+{
+    //User-defined exception classes are derived from the Exception class
+    public TempIsZeroException(string message) : base(message)
+    {
+
+    }
+}
+
+public class Temperature
+{
+    int temperature = 0; 
+
+    public void showTemp()
+    {
+        if (temperature == 0)
+        {
+            throw (new TempIsZeroException("Zero Temperature found"));
+        }
+        else
+        {
+            Console.WriteLine("Temperature: {0}", temperature);
+        }
+    }
+}
